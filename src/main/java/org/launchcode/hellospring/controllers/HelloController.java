9ad749ae64a9eef278782594bbf8e0532a960ddb
katytapley <katy.tapley@gmail.com>
2023-10-02 10:59:02 -0500

    part two

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,10 +2,12 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello")
     @ResponseBody
     public String hello(){
@@ -16,5 +18,12 @@ public class HelloController {
     @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
+    }
+
+    // Create handler that handles requests of the form /hello?name=LaunchCode (query param)
+    @GetMapping("/helloThere")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello there, " + name + "!";
     }
 }
