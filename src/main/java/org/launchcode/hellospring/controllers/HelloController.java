88bc659d031fd55bee1f526cf2d4b989db981315
katytapley <katy.tapley@gmail.com>
2023-10-02 11:28:00 -0500

    part 5, condensing methods

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
     //handle request at path http://localhost:8080/hello
 
@@ -14,28 +16,26 @@ public class HelloController {
 //    }
 
     //handle request at path //http://localhost:8080/goodbye
-    @GetMapping("goodbye")
-    @ResponseBody
+    @GetMapping("goodbye") //now lives at /hello/goodbye
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
+    // now lives at /hello/hellp
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
     //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
-    @ResponseBody
+    // now lives at /hello/form
     public String helloForm() {
         return "<html>" +
                 "<body>" +
